f9d528d73e3e0872957a6b4f1c67d5000d78b685
Vilkov Timur <t.vilkov@unistream.com>
2013-10-03 12:47:43 +0400

    Remove Description from AssemblyInfo

--- src/Inceptum.Workflow/Properties/AssemblyInfo.cs
+++ src/Inceptum.Workflow/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Inceptum.Workflow")]
-[assembly: AssemblyDescription("")]
+//[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("87a3a165-9e10-4d05-a4a6-1fb6b5ed7727")]
