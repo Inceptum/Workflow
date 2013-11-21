194c2a5357aa844223fd713454302d251194857a
Mikhail Tripolitov <mtripolitov@inceptumsoft.com>
2013-11-21 13:09:07 +0400

    Add ToString override for ActivityState

--- src/Inceptum.Workflow/WorkflowExecutor.cs
+++ src/Inceptum.Workflow/WorkflowExecutor.cs
@@ -8,6 +8,11 @@ namespace Inceptum.Workflow
         public dynamic Values { get; set; }
         public string NodeName { get; set; }
         public ActivityResult Status { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("ActivityState for node name {0} status {1}", NodeName, Status);
+        }
     }
 
     class NullExecutionObserver : IExecutionObserver
